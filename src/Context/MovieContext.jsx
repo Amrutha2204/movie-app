4a7bffa82a27f8e80cfed59f818ba48a37e27c8d
Amrutha2204 <nallaamruthareddy1@gmail.com>
2025-11-17 10:54:08 +0530

    Initial Commit

--- src/Context/MovieContext.jsx
+++ src/Context/MovieContext.jsx
@@ -7,8 +7,8 @@ const MovieProvider = ({ children }) => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [isError, setIsError] = useState(false);
-  const [inputTerm, setInputTerm] = useState('the batman');
-  const [searchQuery, setSearchQuery] = useState('the batman');
+  const [inputTerm, setInputTerm] = useState('batman');
+  const [searchQuery, setSearchQuery] = useState('batman');
 
   const fetchMovies = async (url) => {
     setIsLoading(true);
